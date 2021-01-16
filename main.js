c08732c34c7883f1e774fe57d8ba5c0c1230abe0
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-01-16 11:58:07 -0600

    added functinos with arugments and calls to main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("This is for js-intro")
+// console.log("This is for js-intro")
+
+// function sayHello() {
+//   console.log("Hello"); // DECLARED FUNCTION // // This can hoist 
+// }
+
+// const sayHello = function() { // 2nd way to declare function //
+//   console.log("Hello!")
+// }
+
+// const sayHello = () => { // Third way to declare function //
+//   console.log("Hello!")
+// }
+ 
+
+//  sayHello(); // Called Function //
+
+
+// // *******  FUNCTIONS WITH ARGUMENTS ******
+// function logsHello(name) {
+//   console.log('Hello, ${name}!');
+// }
+
+// logsHello('Ben Jarrett');
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log(sum); 
+  return sum;
+}
+
+console.log (addsTwoNumbers(5,7));
