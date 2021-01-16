af4bf3b4fb4b8282bc8c889bc298333b3da921f0
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-01-16 11:08:07 -0600

    added variables, data types, and math to js files

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-console.log("This is for js-intro")
+// console.log("This is for js-intro")
+
+// ********* START VARIABLES **********
+
+let firstName = 'Beyonce';
+let lastName = 'Knowles';
+
+// console.log(firstName + lastName)
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+
+firstName = 'Ben'
+lastName = 'Jarrett'
+
+// console.log(firstName + lastName)
+// console.log(`First Name: ${firstName} Last Name: ${lastName}`)
+
+// const favoriteFood = 'Tacos';
+// console.log(favoriteFood);
+
+// favoriteFood = 'Bread';
+// console.log(favoriteFood)
+
+// ********** END  VARIABLES **********
+
+// ********** START  DATATYPES **********
+// const dog = 'bark'
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14;
+
+// console.log(typeof answer, typeof negative);
+
+// const iLoveJavaScript = true;
+// console.log(typeof iLoveJavaScript);
+
+
+// ********** END  DATATYPES **********
+
+
+// ********** START  MATH **********
+// console.log(Math.pow(2,2)) // 4
+// console.log(Math.pow(3,2)) // 9
+// console.log(Math.pow(3,3)) // 27
+
+// console.log(Math.round(6.5)) // 7
+// console.log(Math.round(6.45)) // 6
+// console.log(Math.floor(6.999)) // 6
+// console.log(Math.ceil(6.00001)) // 7
+
+const cat = 'kitty';
+console.log(cat.length); // 5
+
+// ********** END  MATH **********
